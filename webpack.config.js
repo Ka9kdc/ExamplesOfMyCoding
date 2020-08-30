0619103a53f33ffb4793917e9ab657cb2555ea67
Kelsey Schroeder <kelsey.schroeder@att.net>
2020-08-30 16:43:17 -0500

    fixed issue with babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,14 @@ module.exports = {
         path: path.join(__dirname, '/public'),
         filename: 'bundle.js'
     },
+    //coding from babeljs.io
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      ]
+    },
       // Creates "source map" files (ex. "bundle.js.map"). Modern browsers can automatically
   // request these to "rebuild" your original source code in your dev tools (i.e. the Sources tab).
   // This makes debugging much, much nicer
-    devtool: 'source-map',
-    module: {
-        rules: [
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-          },
-        ],
-      }
-      
+  devtool: 'source-map'
   };
