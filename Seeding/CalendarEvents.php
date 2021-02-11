f8bbfc2458ee4cd1dcafe0f110b5e8dce81f5fad
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-02-11 14:46:29 -0600

    added notes to calendar event seed closes #3

--- Seeding/CalendarEvents.php
+++ Seeding/CalendarEvents.php
@@ -11,16 +11,19 @@ $create_Calendar = "CREATE TABLE IF NOT EXISTS Calendar (
     )";
 
 $nextSundayNetStart = strtotime("Sunday 8:00 PM");
-$nextWeatherStart = strtotime('Tuesday 7:00 pm');
+$nextWeatherStart = strtotime('Second Tuesday of this month 7:00 pm');
 $thismonthClubMeetingStart = strtotime("first friday of this month 7:30PM");
 $thismonthHamfestMeetingStart = strtotime("second thursday of this month 7:30PM");
 $thismonthBoardMeetingStart = strtotime("third thursday of this month 7:30PM");
 $calendarEvents = array();
 
 for($x = 0; $x < 8; $x++){
+    //Makes  8 weekly club nets
     $sql = "INSERT INTO Calendar (eventName, eventStart, eventEnd, EventLocation, EventType) VALUES ('Weekly Club Net', '" . date("Y-m-d H:i:s", strtotime("+" . $x ." week", $nextSundayNetStart)) . "', '" . date("Y-m-d H:i:s", strtotime("+" . $x ." week +15 minutes", $nextSundayNetStart)) . "', '145.310', 'Net')";
     if ($x % 2){
+        // makes 4 biweekly weather net
         $sql2 = "INSERT INTO Calendar (eventName, eventStart, eventEnd, EventLocation, EventType) VALUES ('Weather Net', '" . date("Y-m-d H:i:s", strtotime("+" . $x ." week", $nextWeatherStart)) . "', '" . date("Y-m-d H:i:s", strtotime("+" . $x ." week +15 minutes", $nextWeatherStart)) . "', '444.475', 'Net')";
+        // Makes 4 monthly meetings
         $monthCount = $x / 2;
         $sql3 = "INSERT INTO Calendar (eventName, eventStart, eventEnd, EventLocation, EventType) VALUES ('Monthly Club Meeting', '" . date("Y-m-d H:i:s", strtotime("+" . $monthCount ." week", $thismonthClubMeetingStart)) . "', '" . date("Y-m-d H:i:s", strtotime("+" . $monthCount ." week +2 hours", $thismonthClubMeetingStart)) . "', 'Zoom', 'Meeting')";
         $sql4 = "INSERT INTO Calendar (eventName, eventStart, eventEnd, EventLocation, EventType) VALUES ('Monthly Hamfest Meeting', '" . date("Y-m-d H:i:s", strtotime("+" . $monthCount ." week", $thismonthHamfestMeetingStart)) . "', '" . date("Y-m-d H:i:s", strtotime("+" . $monthCount ." week +1 hours", $thismonthHamfestMeetingStart)) . "', 'Zoom', 'Meeting')";
