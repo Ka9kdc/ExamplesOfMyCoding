f67bd20474ee0181916e842c502887f884b22e76
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-02-12 19:33:26 -0600

    BE: Calendar now fetching this months events

---
+++ phpfiles/getThisMonthsCalanderEvents.php
@@ -0,0 +1,17 @@
+<?php 
+$calendarQuery = "SELECT * FROM Calendar WHERE eventType = 'special Event' OR eventStart BETWEEN '" . date("Y-m-d H:i:s") . "' AND '" . date("Y-m-d H:i:s", strtotime("+1 month")). "' ";
+
+$host = "host = localhost";
+$port = "port = 5432";
+$dbname = "dbname = WCRA_test";
+
+$dbConn = pg_connect( "$host $port $dbname" ) or die('Could not connet:' . pg_last_error());
+
+$result = pg_query($dbConn, $calendarQuery);
+$calendarList = pg_fetch_all($result);
+ 
+echo json_encode($calendarList);
+
+pg_close($dbConn)
+
+?>
