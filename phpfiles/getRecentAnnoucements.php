96fcec7319c0afef3577ab50bab5de72a6f71872
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-02-12 20:12:05 -0600

    BE Announcement get created for all and recent

---
+++ phpfiles/getRecentAnnoucements.php
@@ -0,0 +1,18 @@
+<?php
+
+$announcementsQuery = "SELECT * FROM announcement ORDER BY postDate DESC LIMIT 1";
+
+$host = "host = localhost";
+$port = "port = 5432";
+$dbname = "dbname = WCRA_test";
+
+$dbConn = pg_connect( "$host $port $dbname" ) or die('Could not connet:' . pg_last_error());
+
+$result = pg_query($dbConn, $announcementsQuery);
+$announcementsList = pg_fetch_all($result);
+ 
+echo json_encode($announcementsList);
+
+pg_close($dbConn)
+
+?>
